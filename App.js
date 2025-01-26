f0dcfa9c8667013b7469ae34729048e3eee2034e
chandan9669 <tripathichandan32@gmail.com>
2025-01-26 13:22:09 +0530

    this is day 2

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-/**
- * div
- *    div
- *        H1
- *    div
- * div
- * 
- * */ 
+ 
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 
 
+const Jsxheading= ()=>(
+<h2 id="heading">
+    This is Jsx heading 
+    </h2>
+    );
 
-const heading =React.createElement("div",
-    {id:"parent:"}
-    ,React.createElement("div",
-        {},React.createElement("h1",
-            {},"Hello from react")));
+//React Functional component
+
+const HeadingComponent = ()=> (
+    <div>
+    < Jsxheading />
+     <h1>
+         Hi, I am React functional component 
+         </h1>
+         </div>
+);
+
 const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+const root2=ReactDOM.createRoot(document.getElementById("chandan"));
+
+root.render(<Jsxheading />);
+root2.render(< HeadingComponent />);
 
